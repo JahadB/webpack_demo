e5eccfeecf132f7bce16a2113150f700c20aaf03
JahadB <beylercahad@gmail.com>
2024-06-28 11:33:47 +0400

    ADD extra example about importing and exporting modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 
+import myName from './myName';
+
 function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
+    // use your function!
+    element.textContent = myName('Cody');
+
     return element;
   }
   
